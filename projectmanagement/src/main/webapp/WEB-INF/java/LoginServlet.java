7d21f8d86b7d5f9fae3abb17e1cd12dce10f8493
Felix-PC\Felix <felix.lbarth@gmail.com>
2018-07-12 13:48:15 +0300

    Added html, css, and js from pmt (WebStorm) project.

---
+++ projectmanagement/src/main/webapp/WEB-INF/java/LoginServlet.java
@@ -0,0 +1,32 @@
+import Entity.User;
+import persistence.DataProvider;
+import persistence.HibernateDataProvider;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+        DataProvider dataProvider = new HibernateDataProvider();
+        User user = dataProvider.getUserByUsername(username);
+        if(user != null && user.hasPassword(password)) {
+            resp.sendRedirect("index_user.html");
+        }
+        else {
+            resp.sendRedirect("index.html");
+        }
+
+    }
+}
