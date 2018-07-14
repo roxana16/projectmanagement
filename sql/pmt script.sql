a3ca445c56ba177280fa2398d05a89912dcffdcb
Felix-PC\Felix <felix.lbarth@gmail.com>
2018-07-15 00:10:08 +0300

    Simplified database structure.

--- sql/pmt script.sql
+++ sql/pmt script.sql
@@ -4,10 +4,10 @@ USE `projectmanagement`;
 
 CREATE TABLE IF NOT EXISTS User(
      userId INT NOT NULL AUTO_INCREMENT,
-     userName VARCHAR(255) NOT NULL,
-     firstName VARCHAR(255) NOT NULL,
-     lastName VARCHAR(255) NOT NULL,
-     email VARCHAR(255) NOT NULL,
+     userName VARCHAR(50) NOT NULL,
+     firstName VARCHAR(50) NOT NULL,
+     lastName VARCHAR(50) NOT NULL,
+     email VARCHAR(100) NOT NULL,
      userPass VARCHAR(255) NOT NULL,
      isAdmin BIT DEFAULT 0,
      PRIMARY KEY (userId),
@@ -16,32 +16,10 @@ CREATE TABLE IF NOT EXISTS User(
 
 CREATE TABLE IF NOT EXISTS Project (
      projectId INT NOT NULL AUTO_INCREMENT,
-     projectContent VARCHAR(255),
-     projectTitle VARCHAR(255) NOT NULL,
-     releaseDate DATETIME,
+     projectContent VARCHAR(2100),
+     projectTitle VARCHAR(100) NOT NULL,
+     releaseDate DATE,
      PRIMARY KEY (projectId)
-);
-
-CREATE TABLE IF NOT EXISTS Task(
-     taskId INT NOT NULL AUTO_INCREMENT,
-     taskContent VARCHAR(255),
-     taskName VARCHAR(255) NOT NULL,
-     startTime DATETIME,
-     comment VARCHAR(255) NOT NULL,
-     remainingHours INT,
-     PRIMARY KEY (taskId)
-);	
-
-CREATE TABLE IF NOT EXISTS Files (
-     filesId INT NOT NULL AUTO_INCREMENT,
-     filesContent VARCHAR(255) NOT NULL,
-     PRIMARY KEY (filesId)
-);
-
-CREATE TABLE IF NOT EXISTS Report (
-     reportId INT NOT NULL AUTO_INCREMENT,
-     reportContent VARCHAR(255) NOT NULL,
-     PRIMARY KEY (reportId)
 );
 
 CREATE TABLE IF NOT EXISTS Status (
@@ -50,73 +28,62 @@ CREATE TABLE IF NOT EXISTS Status (
      PRIMARY KEY (statusId)
 );
 
-CREATE TABLE IF NOT EXISTS Comment (
-     commentId INT NOT NULL AUTO_INCREMENT,
-     commentContent VARCHAR(2100) NOT NULL,
-     timeMade DATETIME,
-     taskId INT NOT NULL,
-     userId INT NOT NULL,
-     PRIMARY KEY (commentId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
-     FOREIGN KEY (userId) REFERENCES User(userId)
-);
-
-CREATE TABLE IF NOT EXISTS createProj (
+CREATE TABLE IF NOT EXISTS Task(
+     taskId INT NOT NULL AUTO_INCREMENT,
+     taskContent VARCHAR(2100),
+     taskName VARCHAR(100) NOT NULL,
+     startTime DATETIME,
+     remainingHours INT,
+     userId INT,
      projectId INT NOT NULL,
-     adminId INT NOT NULL,
-     FOREIGN KEY (projectId) REFERENCES Project(projectId),
-     FOREIGN KEY (adminId) REFERENCES User(userId),
-     PRIMARY KEY (projectId, adminId)
-);
-
-CREATE TABLE IF NOT EXISTS createTask (
-     userId INT NOT NULL,
-     taskId INT NOT NULL,
-     FOREIGN KEY (userId) REFERENCES User(userId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
-     PRIMARY KEY (userId, taskId)
-);
-
-CREATE TABLE IF NOT EXISTS projectTask (
-     projectId INT NOT NULL,
-     taskId INT NOT NULL,
-     FOREIGN KEY (projectId) REFERENCES Project(projectId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
-     PRIMARY KEY (projectId, TaskId)
-);
-
-CREATE TABLE IF NOT EXISTS attachFile (
-     filesId INT NOT NULL,
-     taskId INT NOT NULL,
-     FOREIGN KEY (filesId) REFERENCES Files(filesId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
-     PRIMARY KEY (filesId, TaskId)
-);
-
-CREATE TABLE IF NOT EXISTS generateStatus (
      statusId INT NOT NULL,
-     taskId INT NOT NULL,
-     FOREIGN KEY (statusId) REFERENCES Status(statusId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
-     PRIMARY KEY (statusId, TaskId)
+     PRIMARY KEY (taskId),
+     FOREIGN KEY (userId) REFERENCES User(userId) ON DELETE SET NULL,
+     FOREIGN KEY (projectId) REFERENCES Project(projectId) ON DELETE CASCADE,
+     FOREIGN KEY (statusId) REFERENCES Status(statusId) ON DELETE RESTRICT
+);	
+
+CREATE TABLE IF NOT EXISTS File (
+     fileId INT NOT NULL AUTO_INCREMENT,
+     fileName VARCHAR(255) NOT NULL,
+     PRIMARY KEY (fileId)
 );
 
-CREATE TABLE IF NOT EXISTS generateReport (
-     reportId INT NOT NULL,
-     userId INT NOT NULL,
-     generatorId INT NOT NULL,
-     FOREIGN KEY (reportId) REFERENCES Report(reportId),
-     FOREIGN KEY (userId) REFERENCES User(userId),
-	 FOREIGN KEY (generatorId) REFERENCES User(userId),
+CREATE TABLE IF NOT EXISTS Report (
+     reportId INT NOT NULL AUTO_INCREMENT,
+     reportContent VARCHAR(2100) NOT NULL,
+     userId INT,
+     generatorId INT,
+     FOREIGN KEY (userId) REFERENCES User(userId) ON DELETE SET NULL,
+	 FOREIGN KEY (generatorId) REFERENCES User(userId) ON DELETE SET NULL,
      PRIMARY KEY (reportId)
 );
 
-CREATE TABLE IF NOT EXISTS reportTask (
+CREATE TABLE IF NOT EXISTS Comment (
+     commentId INT NOT NULL AUTO_INCREMENT,
+     commentContent VARCHAR(2100) NOT NULL,
+     timeMade DATETIME NOT NULL,
+     taskId INT NOT NULL,
+     userId INT NOT NULL,
+     PRIMARY KEY (commentId),
+     FOREIGN KEY (taskId) REFERENCES Task(taskId) ON DELETE CASCADE,
+     FOREIGN KEY (userId) REFERENCES User(userId) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Task_File (
+     fileId INT NOT NULL,
+     taskId INT NOT NULL,
+     FOREIGN KEY (fileId) REFERENCES File(fileId) ON DELETE CASCADE,
+     FOREIGN KEY (taskId) REFERENCES Task(taskId) ON DELETE CASCADE,
+     PRIMARY KEY (fileId, TaskId)
+);
+
+CREATE TABLE IF NOT EXISTS Report_Task (
      taskId INT NOT NULL,
      statusId INT NOT NULL,
      reportId INT NOT NULL,
-     FOREIGN KEY (reportId) REFERENCES Report(reportId),
-     FOREIGN KEY (statusId) REFERENCES Status(statusId),
-     FOREIGN KEY (taskId) REFERENCES Task(taskId),
+     FOREIGN KEY (reportId) REFERENCES Report(reportId) ON DELETE CASCADE,
+     FOREIGN KEY (statusId) REFERENCES Status(statusId) ON DELETE RESTRICT,
+     FOREIGN KEY (taskId) REFERENCES Task(taskId) ON DELETE CASCADE,
      PRIMARY KEY (reportId, taskId)
 );
